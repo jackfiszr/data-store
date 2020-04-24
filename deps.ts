a345eff56a99068d8207a1d71f0b28b3a426789a
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-04-24 23:51:06 +0200

    add deps, mod, rename index

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export * as fs from "https://deno.land/std@v0.41.0/fs/mod.ts";
+export * as path from "https://deno.land/std@v0.41.0/path/mod.ts";
+export { default as isObject } from "https://raw.githubusercontent.com/jonschlinkert/isobject/master/index.js";
---
+++ mod.ts
@@ -0,0 +1,4 @@
+export * from "./get_value.js";
+export * from "./set_value.js";
+export * from "./utils.js";
+export * from "./store.js";
